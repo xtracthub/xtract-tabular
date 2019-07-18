c5c97286b9f8781c998bdab2d10f2ffb6764a44f
Ubuntu <ubuntu@ip-172-31-71-64.ec2.internal>
2019-07-18 15:19:54 +0000

    Changed script input format to ./run.sh /some/dir file_name --args

--- run.sh
+++ run.sh
@@ -2,16 +2,9 @@
 
 IMAGE_NAME='xtract_tabular_image'
 
-#DIRECTORY=$1
-#FILENAME=$2
-#CHUNKSIZE=${3:-10000}
-
-#echo $CHUNKSIZE
 args_array=("$@")
 DIRECTORY=("${args_array[@]:0:1}")
-echo "${DIRECTORY[@]}"   
-CMD_ARGS=("${args_array[@]:1}")
-echo "${CMD_ARGS[@]}"
-docker run -it -v "${DIRECTORY[@]}":/"${DIRECTORY[@]}" $IMAGE_NAME "${CMD_ARGS[@]}"
+FILE_NAME=("${args_array[@]:1:1}")
+CMD_ARGS=("${args_array[@]:2}")
 
-
+docker run -v $DIRECTORY:/$DIRECTORY $IMAGE_NAME --path /$DIRECTORY/$FILE_NAME "${CMD_ARGS[@]}"
